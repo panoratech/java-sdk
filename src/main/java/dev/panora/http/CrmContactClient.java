30298638814def7940775808ee31c17f7e5ac2a0
nael <39710677+naelob@users.noreply.github.com>
2024-01-03 17:19:03 +0100

    SDK update generated by liblab

--- src/main/java/dev/panora/http/CrmContactClient.java
+++ src/main/java/dev/panora/http/CrmContactClient.java
@@ -4,21 +4,17 @@ import dev.panora.exceptions.ApiException;
 import java.util.List;
 
 public interface CrmContactClient {
-  dev.panora.models.GetContactsResponse getContacts(
-    String integrationId,
-    String linkedUserId,
-    Boolean remoteData
-  ) throws ApiException;
-  dev.panora.models.AddContactResponse addContact(
+  String getContacts(String integrationId, String linkedUserId, Boolean remoteData)
+    throws ApiException;
+  String addContact(
     dev.panora.models.UnifiedContactInput input,
     String integrationId,
     String linkedUserId,
     Boolean remoteData
   ) throws ApiException;
   String updateContact(String id) throws ApiException;
-  dev.panora.models.GetContactResponse getContact(String id, Boolean remoteData)
-    throws ApiException;
-  dev.panora.models.AddContactsResponse addContacts(
+  String getContact(String id, Boolean remoteData) throws ApiException;
+  String addContacts(
     java.util.List<dev.panora.models.UnifiedContactInput> input,
     String integrationId,
     String linkedUserId,
